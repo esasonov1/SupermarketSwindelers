c1d9b6b2b693a19c7e46dbe125b644d4e53d81e5
roseyang48 <ryang48.f@gmail.com>
2024-02-16 16:13:05 -0500

    fixed sprites, added player and movement

---
+++ MidtermProj/Assets/Scripts/GameHandler.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameHandler : MonoBehaviour
+{
+    public List<string> cart = new List<string>();
+    public List<string> pockets = new List<string>();
+    public List<string> order = new List<string>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
