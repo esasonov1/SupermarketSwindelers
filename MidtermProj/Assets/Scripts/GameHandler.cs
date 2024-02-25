1f79fd4efc7dd9340295d963fb9a732e703bf252
avadelaney <103330426+avadelaney@users.noreply.github.com>
2024-02-25 00:57:36 -0500

    added tutorial/more crates to shop floor

--- MidtermProj/Assets/Scripts/GameHandler.cs
+++ MidtermProj/Assets/Scripts/GameHandler.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameHandler : MonoBehaviour
 {
@@ -8,10 +10,12 @@ public class GameHandler : MonoBehaviour
     public List<string> pockets = new List<string>();
     public List<string> order = new List<string>();
     private List<string> collectedItems = new List<string>();
+    public GameObject Tutorial;
+    public GameObject buttonOpenTutorial;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Tutorial.SetActive(false);
     }
 
     public void AddItem(bool theft, string name)
@@ -37,6 +41,21 @@ public class GameHandler : MonoBehaviour
         
         return null;
     }
+
+    public void playGame() {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void Button_OpenTutorial(){
+        Tutorial.SetActive(true);
+        buttonOpenTutorial.SetActive(false);
+    }
+
+    public void Button_CloseTutorial() {
+        Tutorial.SetActive(false);
+        buttonOpenTutorial.SetActive(true);
+    }
+
 
     // Update is called once per frame
     void Update()
