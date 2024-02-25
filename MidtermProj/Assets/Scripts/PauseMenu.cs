b0fb9fce71e4cfe4a558cbcd38a61c11dc64154c
cheyanneatole <cheyanneatole617@gmail.com>
2024-02-25 15:48:13 -0500

    PauseMenu added
    Space is pause

---
+++ MidtermProj/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quiting game...");
+        SceneManager.LoadScene("StartScene");
+    }
+
+}
