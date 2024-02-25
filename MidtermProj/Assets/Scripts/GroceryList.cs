a185341c53ce9af7e038b4fc0578a5090ec7ef49
avadelaney <103330426+avadelaney@users.noreply.github.com>
2024-02-24 23:29:20 -0500

    Added grocery list UI/prefab

---
+++ MidtermProj/Assets/Scripts/GroceryList.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GroceryList : MonoBehaviour{
+
+      public GameObject listMenuUI;
+      public GameObject buttonOpenList;
+
+
+      void Start (){
+            listMenuUI.SetActive(false);
+      }
+
+      void Update (){
+      }
+
+      //Button Functions:
+      public void Button_OpenList(){
+           listMenuUI.SetActive(true);
+           buttonOpenList.SetActive(false);
+      }
+
+      public void Button_CloseList() {
+           listMenuUI.SetActive(false);
+           buttonOpenList.SetActive(true);
+      }
+}
+
