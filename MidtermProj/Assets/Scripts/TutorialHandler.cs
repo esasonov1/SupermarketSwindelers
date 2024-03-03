5310b64101e1ec6ddc8594d17306f6b903c7a270
avadelaney <103330426+avadelaney@users.noreply.github.com>
2024-03-03 16:52:23 -0500

    Fixed/updated tutorial page

---
+++ MidtermProj/Assets/Scripts/TutorialHandler.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialHandler : MonoBehaviour
+
+{
+    public GameObject tutorialScreenUI;
+    public GameObject buttonOpenTutorial;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tutorialScreenUI.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Button_OpenTutorial(){
+        tutorialScreenUI.SetActive(true);
+        buttonOpenTutorial.SetActive(false);
+    }
+
+    public void Button_CloseTutorial() {
+        tutorialScreenUI.SetActive(false);
+        buttonOpenTutorial.SetActive(true);
+    }
+}
